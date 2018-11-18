55241e4ccc9b5a90e2bd1f5c1eda91c64c36e610
yotammanne <yotammanne@mail.tau.ac.il>
2018-11-18 19:14:28 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Simulator
+//
+//  Created by Yotam Manne on 18/11/2018.
+//  Copyright © 2018 Yotam Manne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
